1f5be35f36fa3212d8b342473348fb752aa3134c
Alja Čekada <alja.cekada@gmail.com>
2023-10-21 23:49:35 -0700

    Create apiConstatnts.ts

---
+++ app/constants/apiConstatnts.ts
@@ -0,0 +1,13 @@
+export enum apiRoutes {
+    LOGIN = '/auth/login',
+    SIGNUP = '/auth/register',
+    SIGNOUT = '/auth/signout',
+    REFRESH_TOKENS = '/auth/refresh',
+    FETCH_USER = '/auth',
+    FETCH_USERS = '/users',
+    USERS_PREFIX = '/users',
+    UPLOAD_AVATAR_IMAGE = '/users/upload',
+    UPLOAD_PRODUCT_IMAGE = '/products/upload',
+    // Check this route
+    PRODUCTS_PREFIX = '/auctions',
+  }
