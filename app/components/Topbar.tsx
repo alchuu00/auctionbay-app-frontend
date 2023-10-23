13974cae0bcbe8ef3fb6656a83bc578ffaa3351a
Alja Čekada <alja.cekada@gmail.com>
2023-10-22 22:39:53 -0700

    Create Topbar.tsx

---
+++ app/components/Topbar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Logo from "./Logo";
+import HomeIcon from "@heroicons/react/outline/HomeIcon";
+import UserIcon from "@heroicons/react/outline/UserIcon";
+import UserCircleIcon from "@heroicons/react/outline/UserCircleIcon";
+import PlusIcon from "./PlusIcon";
+
+const Topbar = () => {
+  return (
+    <div className="flex justify-between items-center py-5 px-8">
+      <div className="flex items-center w-fit gap-8">
+        <Logo />
+        <div className="flex gap-1 bg-white rounded-full p-1">
+          <div className="flex gap-1 px-3 py-3">
+            <HomeIcon className="h-5 w-5" />
+            <p>Auctions</p>
+          </div>
+          <div className="flex gap-1 px-3 py-3 bg-black text-white rounded-full">
+            <UserIcon className="h-5 w-5" />
+            <p>Profile</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-1 bg-white rounded-full justify-center items-center p-1">
+        <div className="p-3 rounded-full bg-fluoro-yellow">
+          <PlusIcon />
+        </div>
+        <div className="p-3 rounded-full bg-gray-200">
+            <UserCircleIcon className="h-5 w-5" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
