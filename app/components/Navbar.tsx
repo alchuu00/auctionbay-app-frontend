a3d9357c22ed26170d2ed5a7956f4af7c069e367
Alja Čekada <alja.cekada@gmail.com>
2023-10-22 22:39:43 -0700

    create navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Logo from "./Logo";
 
 const Navbar = () => {
   return (
     <div className="w-screen flex justify-between items-center text-md py-5 px-8">
       <div>
-      <Link href="/"><Image src="/logo.png" alt="Logo" width={50} height={50} className="rounded-full"></Image></Link>
+        <Logo />
       </div>
       <div className="flex gap-2 items-center">
-        <Link href="/login" className="font-bold">Log In</Link>
+        <Link href="/login" className="font-bold">
+          Log In
+        </Link>
         <p className="font-light">or</p>
-        <Link href="/register" className="bg-gray-800 text-white py-2 px-4 rounded-2xl">Sign Up</Link>
+        <Link
+          href="/register"
+          className="bg-gray-800 text-white py-2 px-4 rounded-2xl"
+        >
+          Sign Up
+        </Link>
       </div>
     </div>
   );
