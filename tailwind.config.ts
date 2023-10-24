52abff25b722f860a88ece3ce233c31b754c644a
Alja Čekada <alja.cekada@gmail.com>
2023-10-23 21:52:09 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
     extend: {
       colors: {
         'fluoro-yellow': '#F4FF47',
+        'gray-blue': '#EDF4F2',
+        'dark-gray': '#272D2D',
         'background': '#F6F6F4',
       },
       fontFamily: {
