949334be03a07dc1be109e3859c7dd03ef2f5610
Alja Čekada <alja.cekada@gmail.com>
2023-10-25 19:46:06 -0700

    Create useCreateUpdateAuction.ts

---
+++ app/hooks/useCreateUpdateAuction.ts
@@ -0,0 +1,49 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { AuctionType } from '../models/auction';
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+
+export interface CreateUpdateAuctionFields {
+  title: string;
+  description: string;
+  start_price: number;
+  end_date: string;
+}
+
+interface Props {
+  defaultValues?: AuctionType;
+}
+
+export const useCreateUpdateAuctionForm = ({ defaultValues }: Props) => {
+  const CreateUpdateAuctionSchema = Yup.object().shape({
+    title: Yup.string().required('Title is required'),
+    description: Yup.string().required('Description is required'),
+    start_price: Yup.number().required('Start price is required'),
+    end_date: Yup.string().required('End date is required'),
+  });
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm({
+    defaultValues: {
+      title: '',
+      description: '',
+      end_date: '',
+      ...defaultValues,
+    },
+    mode: 'onSubmit',
+    resolver: yupResolver(CreateUpdateAuctionSchema),
+  });
+
+  return {
+    handleSubmit,
+    errors,
+    control,
+  };
+}
+
+export type CreateUpdateAuctionForm = ReturnType<
+  typeof useCreateUpdateAuctionForm
+>;
