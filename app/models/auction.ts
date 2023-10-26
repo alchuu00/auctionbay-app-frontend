1a2f791b49ca07c6b285c478a44ba5672989562b
Alja Čekada <alja.cekada@gmail.com>
2023-10-25 19:46:00 -0700

    Update auction.ts

--- app/models/auction.ts
+++ app/models/auction.ts
@@ -1,8 +1,8 @@
 export type AuctionType = {
-    id: string
-    title: string;
-    description: string;
-    image: string;
-    start_price: number;
-    end_date: string;
-  }
+  id: string;
+  title: string;
+  description: string;
+  start_price: number;
+  end_date: string;
+  image: string;
+};
