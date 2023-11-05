b2065708169d3ddd5069ae33980ac0793122f940
Alja Čekada <alja.cekada@gmail.com>
2023-11-04 21:22:26 -0700

    create reusable component that counts until end of auction

---
+++ app/utils/countdown.ts
@@ -0,0 +1,11 @@
+import { AuctionType } from '../models/auction';
+
+export const countdown = (auction: AuctionType) => {
+  if (new Date(auction.end_date) > new Date()) {
+    const diffInMilliseconds =
+      new Date(auction.end_date).getTime() - new Date().getTime();
+    const diffInHours = diffInMilliseconds / (1000 * 60 * 60);
+    return Math.ceil(diffInHours); // Use Math.ceil to round up to the nearest hour
+  }
+  return null; // Return null if the auction has ended
+};
