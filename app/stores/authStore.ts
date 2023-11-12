2034db837781baf9c17905d563de8a76822246d8
Alja Čekada <alja.cekada@gmail.com>
2023-11-11 22:39:37 -0700

    refactor authStore to LF

--- app/stores/authStore.ts
+++ app/stores/authStore.ts
@@ -1,34 +1,34 @@
-import { makeAutoObservable } from 'mobx'
-import { UserType } from '../models/auth'
-import { userStorage } from '../utils/localStorage'
+import { makeAutoObservable } from "mobx";
+import { UserType } from "../models/auth";
+import { userStorage } from "../utils/userStorage";
 
 export interface AuthContextType {
-  user?: UserType | null
-  userId?: string | null
-  login: () => void
-  signout: () => void
+  user?: UserType | null;
+  userId?: string | null;
+  login: () => void;
+  signout: () => void;
 }
 
 class AuthStore {
-  user?: UserType | null = userStorage.getUser() || null
-  userId?: string | null = userStorage.getUserId() || null
+  user?: UserType | null = userStorage.getUser() || null;
+  userId?: string | null = userStorage.getUserId() || null;
 
   constructor() {
-    makeAutoObservable(this)
+    makeAutoObservable(this);
   }
 
   login(user: UserType) {
-    userStorage.setUser(user)
-    userStorage.setUserId(user.id)
-    this.user = user
+    userStorage.setUser(user);
+    userStorage.setUserId(user.id);
+    this.user = user;
   }
 
   signout() {
-    userStorage.clearUser()
-    this.user = undefined
-    this.userId = undefined
+    userStorage.clearUser();
+    this.user = undefined;
+    this.userId = undefined;
   }
 }
 
-const authStore = new AuthStore()
-export default authStore
+const authStore = new AuthStore();
+export default authStore;
