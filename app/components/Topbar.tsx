faa7b35bceb824e174cd0eb20f68d957b7eeebfa
Alja Čekada <alja.cekada@gmail.com>
2023-11-11 22:40:27 -0700

    improve topbar UI

--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -4,17 +4,19 @@ import HomeIcon from "@heroicons/react/outline/HomeIcon";
 import UserIcon from "@heroicons/react/outline/UserIcon";
 import UserCircleIcon from "@heroicons/react/outline/UserCircleIcon";
 import PlusIcon from "./PlusIcon";
-import AddAuctions from "./AddAuction";
-import ProfileSettings from "./ProfileSettings";
-import { userStorage } from "../utils/localStorage";
+import AddAuctionsForm from "./AddEditAuctionForm";
+import ProfileSettingsLogout from "./ProfileSettingsLogout";
 import { UserType } from "../models/auth";
 import Tab from "./Tab";
+import Image from "next/image";
+import { useFetchUser } from "../hooks/useFetchUser";
 
 interface Props {
   activeTab: number;
   setActiveTab: (index: number) => void;
   activeTopTab: number;
   setActiveTopTab: (index: number) => void;
+  showAuctionDetails: boolean;
 }
 
 const Topbar: FC<Props> = ({
@@ -22,11 +24,12 @@ const Topbar: FC<Props> = ({
   setActiveTab,
   activeTopTab,
   setActiveTopTab,
+  showAuctionDetails,
 }) => {
-  const [showAddAuctions, setShowAddAuctions] = useState<boolean>(false);
+  const [showAddAuctionsForm, setShowAddAuctionsForm] =
+    useState<boolean>(false);
   const [showProfileSettings, setShowProfileSettings] =
     useState<boolean>(false);
-  const [user, setUser] = useState<UserType | null>(null);
 
   const handleActiveTopTab = (index: number) => {
     setActiveTopTab(index);
@@ -37,25 +40,14 @@ const Topbar: FC<Props> = ({
   };
 
   const handleAddAuctionsClick = () => {
-    setShowAddAuctions(true);
+    setShowAddAuctionsForm(true);
   };
 
   const handleUpdateProfile = () => {
     setShowProfileSettings(true);
   };
 
-  useEffect(() => {
-    const getUser = async () => {
-      try {
-        const user = await userStorage.getUser();
-        setUser(user);
-      } catch (error) {
-        console.error("Error retrieving user:", error);
-      }
-    };
-
-    getUser();
-  }, []);
+  const user = useFetchUser();
 
   return (
     <div className="flex flex-col pt-8">
@@ -65,7 +57,7 @@ const Topbar: FC<Props> = ({
             <Logo />
             <div className="flex gap-1 bg-white rounded-full p-1">
               <div
-                className={`flex gap-1 px-3 py-3 rounded-full ${
+                className={`flex gap-1 px-3 py-3 rounded-full cursor-pointer ${
                   activeTopTab === 1 ? "bg-dark-gray text-white" : ""
                 }`}
                 onClick={() => handleActiveTopTab(1)}
@@ -74,7 +66,7 @@ const Topbar: FC<Props> = ({
                 <p>Auctions</p>
               </div>
               <div
-                className={`flex gap-1 px-3 py-3 rounded-full ${
+                className={`flex gap-1 px-3 py-3 rounded-full cursor-pointer ${
                   activeTopTab === 2 ? "bg-dark-gray text-white" : ""
                 }`}
                 onClick={() => handleActiveTopTab(2)}
@@ -94,55 +86,71 @@ const Topbar: FC<Props> = ({
             </div>
             <div
               onClick={handleUpdateProfile}
-              className="p-3 rounded-full bg-gray-blue cursor-pointer"
+              className="rounded-full cursor-pointer"
             >
-              <UserCircleIcon className="h-5 w-5" />
+              {user?.data.avatar ? (
+                <Image
+                  alt="avatar"
+                  src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/files/${user.data.avatar}`}
+                  width={50}
+                  height={50}
+                  className="rounded-full"
+                />
+              ) : (
+                <div className="p-3 bg-gray-blue rounded-full">
+                  <UserCircleIcon className="h-5 w-5" />
+                </div>
+              )}
             </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col py-4 gap-4 w-full mt-4">
-        {activeTopTab === 1 && (
-          <div className="font-bold text-4xl">Auctions</div>
-        )}
-        {activeTopTab === 2 && (
-          <div>
-            <h1 className="font-bold text-4xl">
-              Hello {user?.user.first_name} {user?.user.last_name} !
-            </h1>
-            <div className="w-full flex flex-col justify-center items-center">
-              <div className="w-fit flex justify-center items-center gap-2 p-1 rounded-2xl bg-gray-blue">
-                <Tab
-                  active={activeTab === 0}
-                  onClick={() => handleActiveTab(0)}
-                >
-                  My auctions
-                </Tab>
-                <Tab
-                  active={activeTab === 1}
-                  onClick={() => handleActiveTab(1)}
-                >
-                  Bidding
-                </Tab>
-                <Tab
-                  active={activeTab === 2}
-                  onClick={() => handleActiveTab(2)}
-                >
-                  Won
-                </Tab>
+      {!showAuctionDetails && (
+        <div className="flex flex-col py-4 gap-4 w-full mt-4">
+          {activeTopTab === 1 && (
+            <div className="font-bold text-4xl">Auctions</div>
+          )}
+          {activeTopTab === 2 && (
+            <div>
+              <h1 className="font-bold text-4xl">
+                Hello {user?.data.first_name} {user?.data.last_name} !
+              </h1>
+              <div className="w-full flex flex-col justify-center items-center">
+                <div className="w-fit flex justify-center items-center gap-2 p-1 rounded-2xl bg-gray-blue">
+                  <Tab
+                    active={activeTab === 0}
+                    onClick={() => handleActiveTab(0)}
+                  >
+                    My auctions
+                  </Tab>
+                  <Tab
+                    active={activeTab === 1}
+                    onClick={() => handleActiveTab(1)}
+                  >
+                    Bidding
+                  </Tab>
+                  <Tab
+                    active={activeTab === 2}
+                    onClick={() => handleActiveTab(2)}
+                  >
+                    Won
+                  </Tab>
+                </div>
               </div>
             </div>
-          </div>
-        )}
-      </div>
-      {showAddAuctions && (
-        <AddAuctions
-          showAddAuctions={showAddAuctions}
-          setShowAddAuctions={setShowAddAuctions}
+          )}
+        </div>
+      )}
+
+      {showAddAuctionsForm && (
+        <AddAuctionsForm
+          showAddAuctionsForm={showAddAuctionsForm}
+          setShowAddAuctionsForm={setShowAddAuctionsForm}
+          isUpdateAuction={false}
         />
       )}
       {showProfileSettings && (
-        <ProfileSettings
+        <ProfileSettingsLogout
           showProfileSettings={showProfileSettings}
           setShowProfileSettings={setShowProfileSettings}
         />
