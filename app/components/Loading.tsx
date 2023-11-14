22462ab9777d81c2ddaecb482e715f80aebfaaaa
Alja Čekada <alja.cekada@gmail.com>
2023-11-13 20:35:51 -0700

    style spinner component

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="w-full flex justify-center items-center my-40">
+    <div className="w-screen h-screen flex justify-center items-center">
       <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-fluoro-yellow" />
     </div>
   );
