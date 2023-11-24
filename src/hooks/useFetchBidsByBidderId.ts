86929152c87da404c06f1ed666106723dc1e088f
Alja Čekada <alja.cekada@gmail.com>
2023-11-24 11:52:25 -0700

    add forbidden and unauthorized response handling

--- src/hooks/useFetchBidsByBidderId.ts
+++ src/hooks/useFetchBidsByBidderId.ts
@@ -1,13 +1,23 @@
 import { useEffect, useState } from 'react';
 import * as API from '../api/api';
+import { useRouter } from 'next/navigation';
+import { StatusCode } from '../constants/errorConstants';
 
 export const useFetchBidsByBidderId = (bidderId: string) => {
   const [bids, setBids] = useState(null);
 
+  const router = useRouter();
+
   const fetchBids = async () => {
     try {
       const bidsData = await API.getBidsByBidderId(bidderId);
-      setBids(bidsData);
+      if (bidsData.data?.statusCode === StatusCode.FORBIDDEN) {
+        router.push("/");
+      } else if (bidsData.data?.statusCode === StatusCode.UNAUTHORIZED) {
+        router.push("/");
+      } else {
+        setBids(bidsData);
+      }
     } catch (error) {
       console.error("Error fetching bids data: ", error);
     }
