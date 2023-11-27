fa7e7547446a06b71438ded90d850b6c8dcfcba9
Alja Čekada <alja.cekada@gmail.com>
2023-11-27 12:41:54 -0700

    change api routes for reset password

--- src/api/passwordReset.ts
+++ src/api/passwordReset.ts
@@ -2,22 +2,23 @@ import { apiRoutes } from "../constants/apiConstatnts";
 import { ResetPasswordFields } from "../hooks/useResetPassword";
 import { apiRequest } from "./api";
 
-  export const sendPasswordResetEmail = async (email: string) =>
+export const sendPasswordResetEmail = async (email: string) =>
   apiRequest<{ email: string }, void>(
     "post",
     `${apiRoutes.AUTH}/send-password-reset-email`,
     { email }
   );
 
-  export const getUserFromToken = async (token: string) =>
-  apiRequest<void, void>(
-    "get",
-    `${apiRoutes.RESET_PASSWORD}/${token}`
-  );
+export const getUserFromToken = async (token: string) =>
+  apiRequest<void, void>("get", `${apiRoutes.RESET_PASSWORD}/${token}`);
 
-  export const resetUserPassword = async (data: Omit<ResetPasswordFields, 'new_password'>, id: string) =>
-  apiRequest<ResetPasswordFields, void>(
+export const resetUserPassword = async (
+  updateUserDto: ResetPasswordFields,
+  id: string,
+  token: string
+) =>
+  apiRequest<{updateUserDto: ResetPasswordFields, token: string}, void>(
     "patch",
     `${apiRoutes.RESET_PASSWORD}/${id}`,
-    data
+    {updateUserDto, token}
   );
