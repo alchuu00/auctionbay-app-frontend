3ac1bd255d3c7978873d5c174921a761273b814a
Alja Čekada <alja.cekada@gmail.com>
2023-12-12 18:30:44 -0800

    Update image domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const nextConfig = {};
 
 module.exports = {
   images: {
-    domains: ["localhost:8080", 'localhost'],
+    domains: ["localhost:8080", "localhost", "auctionbay-backend.onrender.com"],
   },
 };
