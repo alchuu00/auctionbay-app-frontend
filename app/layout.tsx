7c480c99bb34b3ab059e6fc19bb844a07de7d20f
Alja Čekada <alja.cekada@gmail.com>
2023-12-14 12:52:16 -0800

    Add React Toastify CSS import

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "../src/utils/provider";
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
